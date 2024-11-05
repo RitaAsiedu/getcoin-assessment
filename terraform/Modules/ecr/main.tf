b297608bfd573eb27ea33ae9686dfbfe3882bf09
Rita Asiedu <rasiedu202@gmail.com>
2024-11-05 12:01:32 -0500

    creating ecr repo

---
+++ terraform/Modules/ecr/main.tf
@@ -0,0 +1,11 @@
+resource "aws_ecr_repository" "app" {
+  name = var.repository_name
+  
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+} 
+
+output "repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
