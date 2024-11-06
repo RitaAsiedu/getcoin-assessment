983800f85d683000d0e74f8414c1ac006af827f0
Rita Asiedu <rasiedu202@gmail.com>
2024-11-05 23:28:36 -0500

    modified/added terraform modules

---
+++ terraform/modules/k8s-app/variables.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+    default = "us-east-1"
+}
+
+variable "app_name" {
+    description     = "Name of the application"
+    type    = string
+}
+
+variable "availability_zones" {
+    default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable    "eks_cluster_name"{
+    description = "Name of the EKS cluster"
+    type        = string
+}
+
+variable "image_url" {
+    description = "Image URL"
+    type        = string
+}
---
+++ terraform/modules/s3/variables.tf
@@ -0,0 +1,15 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = "cryptos3bucket1"
+} 
+
+variable "state_bucket" {
+  description = "Name of the S3 bucket for Terraform state"
+  type        = string
+}
+
+variable "state_key" {
+  description = "Key for the Terraform state file in the S3 bucket"
+  type        = string
+}
