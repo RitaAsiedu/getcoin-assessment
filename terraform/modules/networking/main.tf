7dd5871f79e4faf0135b51726128d25cb113f7d6
Rita Asiedu <rasiedu202@gmail.com>
2024-11-07 00:41:50 -0500

    added security groups

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -179,6 +179,19 @@ resource "aws_security_group" "eks_cluster" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
